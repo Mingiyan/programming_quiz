26e8ade91beb8083961a5bd156d9930b47ee0bda
Mingiyan Dordzhiev <vvvdmingiyan@gmail.com>
2022-04-04 20:48:21 +0300

    added commands

---
+++ src/main/java/com/kalmyk/service/command/HelpCommand.java
@@ -0,0 +1,36 @@
+package com.kalmyk.service.command;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Service
+@RequiredArgsConstructor
+public class HelpCommand implements Command {
+
+    @Override
+    public String commandDescription() {
+        return null;
+    }
+
+    @Override
+    public String commandName() {
+        return null;
+    }
+
+    @Override
+    public BotApiMethod execute(SessionContext context) {
+        return null;
+    }
+
+    @Override
+    public BotApiMethod process(SessionContext context, Update update) {
+        return null;
+    }
+
+    @Override
+    public boolean isPublic() {
+        return false;
+    }
+}
